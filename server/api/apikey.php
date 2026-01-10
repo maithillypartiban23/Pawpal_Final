1f0dbad8cbc3a428b895b8e4a0c22568e84cef02
Maithilly Partiban <maithilly.partiban0623@gmail.com>
2026-01-11 01:15:05 +0800

    Delete server directory

--- server/api/apikey.php
+++
@@ -1,7 +0,0 @@
-<?php
-$apikey = '3d7e560b-362e-4989-ba30-a2142a0f307e';
-
-$collectionid= '2nmdnuwi';
-
-$xkey= '1ec1ad6b51637dbcf40a0ff377751c497b336350a0dea2c44764a104cdf51dea0414ad31872cebc38ff19cbb3e0c0ec08d782a7f407dc4a01886a8d81150fe0c';
-?>
